f51cfc5836b4deb52138003a44249ed40baef2d9
Ankit Prakash <prakashankit526@gmail.com>
2023-03-14 21:27:27 +0530

    connected with database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const connection = require("./config/db");
 
 const app = express();
+
+connection();
+
+app.get("/health",(req,res) => {
+    res.send(`Up and running at ${new Date()}`);
+});
 
 
 const host = process.env.HOST || "localhost";
